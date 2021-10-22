810c52dd74b16e81b0b0cb531b3f42a85d3ad541
Curtis <Curt122181@gmail.com>
2021-10-22 18:27:58 -0400

    Updated file

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,15 @@
 /*FizzBuzz using a for loop, I was learning while watching Coder Foundry youtube Tutorials.*/
 function FizzBuzzA(value1, value2){
     let returnValue = "";
-    
+
     for(let i = 1; i <= 100; i ++){
         if(i%value1 == 0 && i%value2 == 0){ 
             returnValue += " FizzBuzz!";
         }
-         else if (i%value1 == 0){ 
+         else if (i%value1 == 0){ //default value1 #3.
             returnValue += " fizz";
         }
-        else if (i%value2 == 0){
+        else if (i%value2 == 0){ //default value2 #5.
             returnValue += " buzz";
         }
         else {
