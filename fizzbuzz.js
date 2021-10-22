0ef198656e10a97b09b5010e5f46ed50083ac1c7
Curtis <Curt122181@gmail.com>
2021-10-22 14:31:08 -0400

    Updated files

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,20 @@
-//FizzBuzz using a for loop, I was learning while watching Coder Foundry youtube Tutorial.//
+/*FizzBuzz using a for loop, I was learning while watching Coder Foundry youtube Tutorials.*/
 
 function FizzBuzzA(value1, value2){
     let returnValue = "";
     for(let i = 1; i <= 100; i ++){
-        if(i%value1 == 0 && i%value2 == 0){//if 3% is divisible 0 and and if 5% == 0
-                                          //( % Modulus operator)(Remainder)
+        if(i%value1 == 0 && i%value2 == 0){ 
             returnValue += " FizzBuzz!";
         }
-         else if (i%value1 == 0){
-            returnValue += " Fizz";
+         else if (i%value1 == 0){ /* .*/
+            returnValue += " fizz";
         }
         else if (i%value2 == 0){
-            returnValue += " Buzz";
+            returnValue += " buzz";
         }
         else {
             returnValue += i + " ";
         }
     } 
     return returnValue;
-}// curtis notes: 
+}
