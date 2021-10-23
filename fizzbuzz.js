f5e25a3bbf55347cf3c58665ee10ea038d27f709
Curtis <Curt122181@gmail.com>
2021-10-22 20:52:42 -0400

    Updated files

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,17 +4,26 @@ function FizzBuzzA(value1, value2){
 
     for(let i = 1; i <= 100; i ++){
         if(i%value1 == 0 && i%value2 == 0){ 
-            returnValue += " FizzBuzz!";
+            returnValue +=  "FizzBuzz!!! ";
         }
          else if (i%value1 == 0){ //default value1 #3.
-            returnValue += " fizz";
+            returnValue += "fizz ";
         }
         else if (i%value2 == 0){ //default value2 #5.
-            returnValue += " buzz";
+            returnValue += "buzz ";
         }
         else {
             returnValue += i + " "; //if neither are TRUE print the number.  
         }
     } 
     return returnValue;
+}
+
+// Call and use the FizzBuzzA function
+function buzzIt(){
+    let output="";
+    let valueOne = document.getElementById("Fizzvalue").value;//ERROR
+    let valueTwo = document.getElementById("Buzzvalue").value;//ERROR
+    output = FizzBuzzA(valueOne, valueTwo);
+    document.getElementById("results").innerHTML = output;
 }
